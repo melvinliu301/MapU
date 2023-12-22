63d2ac3243c4c936ccb4f05677dd40029ab5e50b
Melvin Liu <u3579073@connect.hku.hk>
2023-12-23 02:37:57 +0800

    with interval bugs

--- fypapp/src/screens/CameraScreen.jsx
+++ fypapp/src/screens/CameraScreen.jsx
@@ -3,18 +3,12 @@ import React from "react";
 import {
     StyleSheet,
     View,
-    TouchableHighlight,
     Text,
-    TextInput,
-    FlatList,
-    Image,
     TouchableOpacity,
 } from "react-native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Toast from "react-native-toast-message";
 import { useNavigation, useFocusEffect } from "@react-navigation/native";
 import { Camera } from 'expo-camera';
-// import Ionicons from "react-native-vector-icons/Ionicons";
-// import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
 
 
@@ -26,6 +20,7 @@ const CameraScreen = () => {
 
     const cameraRef = useRef(null);
     const captureInterval = useRef(null);
+    const toastInterval = useRef(null);
     const [isCapturing, setIsCapturing] = useState(false);
 
     const [hasPermission, setHasPermission] = useState(null);
@@ -37,6 +32,8 @@ const CameraScreen = () => {
             console.log("CameraScreen focused");
             return () => {
                 setIsCapturing(false);
+                stopCapture();
+                stopToast();
                 console.log("CameraScreen unfocused");
             };
         }, [])
@@ -44,37 +41,66 @@ const CameraScreen = () => {
     
     useEffect(() => {
         (async () => {
-            const { status } = await Camera.requestMicrophonePermissionsAsync();
+            const { status } = await Camera.requestCameraPermissionsAsync();
             setHasPermission(status === "granted");
         })();
     }
     , []);
 
+
     useEffect(() => {
         if (isCapturing) {
             startCapture();
-        } else{
+            startToast();
+        } else {
             stopCapture();
+            stopToast();
         }
-    }, [isCapturing]);
+    }, [isCameraReady, isCapturing]);
 
 
     const onCameraReady = () => {
         setIsCameraReady(true);
+        console.log("Camera ready");
     };
 
     const startCapture = () => {
-        captureInterval.current = setInterval(() => {
-            if (isCameraReady) {captureImage();
+        captureInterval.id = setInterval(() => {
+            if (isCameraReady && isCapturing) {captureImage();
             console.log("Capturing image");}
-          }, 300);
+            console.log("running");
+          }, 500);
     };
 
     const stopCapture = () => {
-        if (captureInterval.current) {
-            clearInterval(captureInterval.current);
-          }
+        if (captureInterval.id) {
+            clearInterval(captureInterval.id);
+            captureInterval.id = null;
+            console.log("Capture stopped");
+        }
     };
+
+    const startToast = () => {
+        toastInterval.id = setInterval(() => {
+            Toast.show({
+                type: "info",
+                position: "top",
+                text1: "Try to find some iconic marks or landmarks",
+                visibilityTime: 3000,
+                autoHide: true,
+            });
+            console.log("Toast Interval");
+        }, 8000);        
+    }
+
+
+    const stopToast = () => {
+        if (toastInterval.id) {
+            clearInterval(toastInterval.id);
+            toastInterval.id = null;
+            console.log("Toast stopped");
+        }
+    }
 
 
     const captureImage = async () => {
@@ -90,7 +116,7 @@ const CameraScreen = () => {
 
     return (
         <View style={styles.container}>
-            <Camera
+            {hasPermission? <Camera
                 ref={cameraRef}
                 style={styles.camera}
                 type={Camera.Constants.Type.back}
@@ -98,7 +124,12 @@ const CameraScreen = () => {
                 ratio={"16:9"}
                 onCameraReady={onCameraReady}
             >
+                <View style={styles.textContainer}>
+                    <Text style={styles.camText}>Move Camera around to capture your surroundings</Text> 
+                </View>
             </Camera>
+            :<Text>No access to camera</Text>}
+            <Toast/>
         </View>
     )
 
@@ -108,19 +139,41 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
+
+    textContainer: {
+        flex: 1,
+        backgroundColor: "transparent",
+        flexDirection: "column",
+        justifyContent: "flex-end",
+        alignItems: "center",
+        marginBottom: 100,
+        
+    },  
+
     camera: {
         flex: 1,
     },
+
     buttonContainer: {
         flex: 1,
         backgroundColor: "transparent",
         flexDirection: "row",
         margin: 20,
     },
+
     button: {
         flex: 1,
         alignSelf: "flex-end",
         alignItems: "center",
+    },
+
+    camText: {
+        fontSize: 16,
+        color: "white",
+        fontWeight: "bold",
+        textAlign: "center",
+        width: "70%",
+        
     },
 });
 
--- fypapp/src/screens/MapScreen.jsx
+++ fypapp/src/screens/MapScreen.jsx
@@ -1,4 +1,3 @@
-import { Toasts, toast } from "@backpackapp-io/react-native-toast";
 // import * as Location from "expo-location";
 import React, { useEffect, useState } from "react";
 import { StyleSheet, View, TouchableOpacity, TextInput, Text, } from "react-native";
@@ -16,13 +15,18 @@ const MapScreen = ({ navigation }) => {
         console.log("Search button pressed");
     }
 
-    const InputArea = () => {
-        return (
+   
+
+    return (
+        <View style={styles.container}>
+
+
             <View style={{flex: 1, flexDirection: "row", justifyContent: "space-between", alignItems: "center", width: "100%", height:'auto', backgroundColor: "#35dba1"}}>
                 <View style={{ flex:1, flexDirection: "column", justifyContent: "space-between", alignItems: "center", width: "100%", height:'auto',  backgroundColor: "#35dba1"}}>
                     <View style={{flex:1, flexDirection: "row", justifyContent: 'flex-start', alignItems: "baseline", width: "100%",  backgroundColor: "#35dba1"}}>
                         <Text style={styles.text}>Current Location</Text>
                         <TextInput
+                            id="locationInput"
                             style={styles.textInput}
                             onChangeText={text => setLocationInput(text)}
                             value={locationInput}
@@ -33,6 +37,7 @@ const MapScreen = ({ navigation }) => {
                     <View style={{flex: 1, flexDirection: "row", justifyContent: 'flex-start', alignItems: "baseline", width: "100%",  backgroundColor: "#35dba1"}}>
                         <Text style={styles.text}>Destination</Text>
                         <TextInput
+                            id="destinationInput"
                             style={styles.textInput}
                             onChangeText={text => setDestinationInput(text)}
                             value={destinationInput}
@@ -47,12 +52,8 @@ const MapScreen = ({ navigation }) => {
                     <Text style={{fontSize: 14, fontWeight: 'bold', color: 'black'}}>Search</Text>
                 </TouchableOpacity>
             </View>
-        )
-    }
 
-    return (
-        <View style={styles.container}>
-            <InputArea />
+
             <MapView
                 style={styles.map}
                 initialRegion={{
@@ -69,12 +70,14 @@ const MapScreen = ({ navigation }) => {
 }
 
 const styles = StyleSheet.create({
+
     container: {
         flex: 1,
         backgroundColor: "#fff",
         alignItems: "center",
         justifyContent: "flex-end",
     },
+
     inputArea: {
         flex: 1,
         flexDirection: "row",
@@ -84,6 +87,7 @@ const styles = StyleSheet.create({
         height: 'auto',
         backgroundColor: "#35dba1",
     },
+
     text: {
         fontSize: 16,
         width: "30%",
@@ -91,6 +95,7 @@ const styles = StyleSheet.create({
         color: "white",
         marginLeft: 10,
     },
+
     textInput: {
         height: 40,
         width: "60%",
@@ -100,6 +105,7 @@ const styles = StyleSheet.create({
         borderRadius: 5,
         marginHorizontal: 5,
     },
+
     searchButton: {
         height: 'auto',
         width: "20%",
@@ -111,6 +117,7 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
     },
+
     map: {
         flex: 4,
         width: "100%",
